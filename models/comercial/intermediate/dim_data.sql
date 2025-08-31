0d3db76c29918c23d4600110b0c4a69700158561
Arnaldo Marquissolo <arnaldomj09@gmail.com>
2025-08-31 15:12:46 +0000

    Correção das duplicatas

--- models/comercial/intermediate/dim_data.sql
+++ models/comercial/intermediate/dim_data.sql
@@ -6,9 +6,16 @@ with TRANSACOES as (
     , tr.nome_transacao
     , tr.valor_transacao
     , ptax.valor_cambio as valor_cambio
-    
+    , row_number() over (partition by tr.pk_transacao order by ptax.data_cambio desc) as rn
     
     from {{ref('stg_transacoes')}} tr
     left join {{ref("stg_ptax")}} ptax on ptax.data_cambio = (select max(data_cambio) from {{ref("stg_ptax")}} where data_cambio <= tr.data_transacao)
 )
-select * from TRANSACOES
+select
+    pk_transacao
+    , fk_conta
+    , data_transacao
+    , nome_transacao
+    , valor_transacao
+    , valor_cambio
+from transacoes qualify rn = 1
